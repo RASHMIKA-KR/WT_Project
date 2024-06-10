3105f65c2c9f23590f0925690facee2ce0e361d5
RASHMIKA-KR <krrashmika2004@gmail.com>
2024-06-10 18:52:16 +0530

    project source code added

---
+++ Backend/models/userSchema.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your Name!"],
+    minLength: [3, "Name must contain at least 3 Characters!"],
+    maxLength: [30, "Name cannot exceed 30 Characters!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your Email!"],
+    validate: [validator.isEmail, "Please provide a valid Email!"],
+  },
+  phone: {
+    type: String,
+    required: [true, "Please enter your Phone Number!"],
+    validate: {
+      validator: function(value) {
+        // Check if the phone number is exactly 10 digits and contains only digits
+        return /^[6-9]\d{9}$/.test(value);
+      },
+      message: "Phone number must be exactly 10 digits long and contain only digits!",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a Password!"],
+    minLength: [8, "Password must contain at least 8 characters!"],
+    maxLength: [32, "Password cannot exceed 32 characters!"],
+    select: false,
+  },
+  role: {
+    type: String,
+    required: [true, "Please select a role"],
+    enum: ["Job Seeker", "Employer", "Data Analyst", "ML Engineer"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Pre-save hook to hash the password before saving
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Method to generate JWT token
+userSchema.methods.getJWTToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+export const User = mongoose.model("User", userSchema);
